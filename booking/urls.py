0e1d577e16258fd03e820a2e1297df0da19afce6
frank <franciskipchumba5@gmail.com>
2017-11-25 17:13:56 +0300

    [FT]Fix navigation links

--- booking/urls.py
+++ booking/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.home_page, name="home"),
     url(r'^categories', views.categories_page, name="categories"),
     url(r'^pricing$', views.pricing, name="pricing"),
-    url(r'^maestro', views.maestro, name="mestro"),
+    url(r'^maestro', views.maestro, name="maestro"),
     url(r'^about', views.about_us, name="about"),
     url(r'^contact/', views.contact, name="contact")
 ]
