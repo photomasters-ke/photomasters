277a9d5797f53a539267929d9a3c157cf3c21af3
frank <franciskipchumba5@gmail.com>
2017-11-25 15:54:42 +0300

    [chore]Create tempalate pages

--- booking/views.py
+++ booking/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 def home_page(request):
     return render(request, 'booking/home.html')
+
+def categories_page(request):
+    return render(request, ,'booking/catgories.html')
+
+
+def pricing(request):
+    return render(request, , 'booking/pricing.html')
+
+
+def maestro(request):
+    return render(request, , 'booking/maestro.html')
+
+
+def about_us(request):
+    return render(request, , 'booking/about_us.html')
+
+def contact(request):
+    return render(request, , 'booking/contact.html')
